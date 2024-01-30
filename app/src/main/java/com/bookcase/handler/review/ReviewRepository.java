5cbef494ed7015ca4a7a6112d7ceda5f536f48c6
tasty13 <bodri0625@gmail.com>
2024-01-31 01:57:40 +0900

    240131

--- app/src/main/java/com/bookcase/handler/review/ReviewRepository.java
+++ app/src/main/java/com/bookcase/handler/review/ReviewRepository.java
@@ -5,4 +5,22 @@ import com.bookcase.vo.Review;
 public class ReviewRepository {
   Review[] reviews = new Review[3];
   int length = 0;
+
+  public void add(Review review) {
+
+  }
+
+  public Review remove(int index) {
+    return null;
+  }
+
+  public Review[] toArray(){
+
+      return new Review[0];
+  }
+
+  public Review get(){
+
+      return null;
+  }
 }
